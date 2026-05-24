88584e39d66023fc87771c6108a4b31f27fefa10
KCroce <kaiodellacroce@gmail.com>
2026-05-24 10:33:19 -0300

    change sbtt

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "Portfólio", // Site title
-	subtitle: "Demo Site",
+	subtitle: "KDCroce's Portfolio", // Site subtitle, displayed below the title
 	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 0, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
